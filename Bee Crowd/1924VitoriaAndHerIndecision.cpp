70352e52ae8d7230f6eafe1dc50227bca12cefcb
nova-nva <alexander.sosa.asillanes@outlook.com>
2022-04-06 17:12:33 -0400

    added some bee crowd basic problems

---
+++ Bee Crowd/1924VitoriaAndHerIndecision.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; cin>>n;
+    string a;
+    for(int i=0; i<n; i++){
+		cout<<"Vuelta nro "<<i<<endl;
+		cin>>a;
+    }
+    cout<<"Ciencia de Computacao"<<endl;
+}
